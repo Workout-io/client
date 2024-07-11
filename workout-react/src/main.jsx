c5927a516b3b3cf30543cd701528b01e8db139f1
danee <danethan@students.cs.ubc.ca>
2024-07-11 23:33:25 +0700

    added material ui

--- workout-react/src/main.jsx
+++ workout-react/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
+
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
       </Routes> 
     </BrowserRouter>
   </React.StrictMode>,
---
+++ workout-react/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import TextField from '@mui/material/TextField';
+
+export default function App() {
+    return(
+    <div>
+        Hi I'm Home
+        <TextField variant="outlined" label="Email" type="email" />
+    </div>
+    );
+}
