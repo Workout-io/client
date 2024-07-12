9b020d2238302d8a04bcb657d58bd369c790eb8c
danee <danethan@students.cs.ubc.ca>
2024-07-12 11:37:24 +0700

    added login

--- workout-react/src/main.jsx
+++ workout-react/src/main.jsx
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme/theme.js'; // Import your custom theme
 
 import "./index.css"
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
       </Routes> 
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
