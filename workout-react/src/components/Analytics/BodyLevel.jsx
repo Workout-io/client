e0d482b867cbcc686bfe0afc5dd7f8fa1ac138be
Jennifer <jennifer.s.e.tjen@gmail.com>
2024-07-13 01:25:44 +0700

    added home widgets

--- workout-react/src/components/Analytics/BodyLevel.jsx
+++ workout-react/src/components/Analytics/BodyLevel.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Abs from '../../assets/Images/Abs.png';
 import AbsandArms from '../../assets/Images/AbsandArms.png';
 import Cungkring from '../../assets/Images/cungkring.png';
 import Box from '@mui/material/Box';
 import { LinearProgress } from '@mui/material';
 
-const BodyLevel = ({ chest, thigh, abs, glutes, arms, back, absGood, armsGood }) => {
+const BodyLevel = () => {
+  const [chest, setChest] = useState(0);
+  const [legs, setLegs] = useState(0);
+  const [abs, setAbs] = useState(0);
+  const [glutes, setGlutes] = useState(0);
+  const [arms, setArms] = useState(0);
+  const [back, setBack] = useState(0);
+  const [absGood, setAbsGood] = useState(false);
+  const [armsGood, setArmsGood] = useState(false);
+  const [loading, setLoading] = useState(true); // State to manage loading state
+  const [error, setError] = useState(null); // State to manage errors
+
   const image = extractImage();
-  const value = 90;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://be-production.vercel.app/api/stats');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        console.log(result.data[0].arm);
+        setArms(result.data[0].arm);
+        setLegs(result.data[0].legs);
+        setAbs(result.data[0].abs);
+        setChest(result.data[0].chest);
+        setGlutes(result.data[0].glutes);
+        setBack(result.data[0].back);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   function extractImage() {
     if (absGood && armsGood) {
@@ -19,83 +54,64 @@ const BodyLevel = ({ chest, thigh, abs, glutes, arms, back, absGood, armsGood })
     }
   }
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div className="flex gap-20 items-center">
-      <div className="w-1/2 h-1/2">
-        <img src={image} alt="Body level" />
+    <div>
+      <div className="font-medium flex flex-row gap-3">
+        Hi, <span className="font-medium">User!</span>
       </div>
-
-      <div className="flex flex-col w-full gap-5">
-      <div className="w-full">
-        <span>Chest</span>
-      <Box sx={{ width: '100%', height: '50%' }}>
-        <LinearProgress
-          variant="determinate"
-          value={chest}
-          sx={{ height: 20 }} // Increase the height here
-        />
-      </Box>
-      </div>
-      
-      <div className="w-full">
-        <span>Thigh</span>
-      <Box sx={{ width: '100%', height: '50%' }}>
-        <LinearProgress
-          variant="determinate"
-          value={thigh}
-          sx={{ height: 20 }} // Increase the height here
-        />
-      </Box>
-      </div>
-
-      <div className="w-full">
-        <span>Abs</span>
-      <Box sx={{ width: '100%', height: '50%' }}>
-        <LinearProgress
-          variant="determinate"
-          value={abs}
-          sx={{ height: 20 }} // Increase the height here
-        />
-      </Box>
-      </div>
-
-      <div className="w-full">
-        <span>Glutes</span>
-      <Box sx={{ width: '100%', height: '50%' }}>
-        <LinearProgress
-          variant="determinate"
-          value={glutes}
-          sx={{ height: 20 }} // Increase the height here
-        />
-      </Box>
-      </div>
-
-      <div className="w-full">
-        <span>Arms</span>
-      <Box sx={{ width: '100%', height: '50%' }}>
-        <LinearProgress
-          variant="determinate"
-          value={arms}
-          sx={{ height: 20 }} // Increase the height here
-        />
-      </Box>
-      </div>
-
-      <div className="w-full">
-        <span>Back</span>
-      <Box sx={{ width: '100%', height: '50%' }}>
-        <LinearProgress
-          variant="determinate"
-          value={back}
-          sx={{ height: 20 }} // Increase the height here
-        />
-      </Box>
-      </div>
-
+      <div className="flex gap-20 items-center">
+        <div className="w-1/2 h-1/2">
+          <img src={image} alt="Body level" />
         </div>
-
+        <div className="flex flex-col w-full gap-5">
+          <div className="w-full">
+            <span>Chest</span>
+            <Box sx={{ width: '100%', height: '50%' }}>
+              <LinearProgress variant="determinate" value={chest} sx={{ height: 20 }} />
+            </Box>
+          </div>
+          <div className="w-full">
+            <span>Legs</span>
+            <Box sx={{ width: '100%', height: '50%' }}>
+              <LinearProgress variant="determinate" value={legs} sx={{ height: 20 }} />
+            </Box>
+          </div>
+          <div className="w-full">
+            <span>Abs</span>
+            <Box sx={{ width: '100%', height: '50%' }}>
+              <LinearProgress variant="determinate" value={abs} sx={{ height: 20 }} />
+            </Box>
+          </div>
+          <div className="w-full">
+            <span>Glutes</span>
+            <Box sx={{ width: '100%', height: '50%' }}>
+              <LinearProgress variant="determinate" value={glutes} sx={{ height: 20 }} />
+            </Box>
+          </div>
+          <div className="w-full">
+            <span>Arms</span>
+            <Box sx={{ width: '100%', height: '50%' }}>
+              <LinearProgress variant="determinate" value={arms} sx={{ height: 20 }} />
+            </Box>
+          </div>
+          <div className="w-full">
+            <span>Back</span>
+            <Box sx={{ width: '100%', height: '50%' }}>
+              <LinearProgress variant="determinate" value={back} sx={{ height: 20 }} />
+            </Box>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default BodyLevel;
--- workout-react/src/components/MainPage.jsx
+++ workout-react/src/components/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './Navbar'
 import Data from './Analytics/Data'
+import WorkoutSummary from './Analytics/WorkoutSummary'
 
 const MainPage = ({num}) => {
   const style = {
@@ -10,8 +11,22 @@ const MainPage = ({num}) => {
   return (      
     <div style={style} className="w-full h-full rounded-xl bg-white drop-shadow-xl px-10 py-5 gap-10">
         <NavBar num={num}/>
-        <div className="h-full w-full flex justify-center items-center">
-            <Data></Data>
+        <div className="h-full w-full flex pt-12 gap-3 justify-center pt-40">
+            <div className="h-fit w-fit bg-stone-100 pb-8 pt-4 rounded-xl">
+              <Data/>
+            </div>
+            <div className="flex flex-col w-52 gap-2">
+            <div className="flex flex-col h-fill w-52 bg-stone-100 py-2 px-2 rounded-xl gap-2">
+              <div className="font-medium">Coming up Next</div>
+              <WorkoutSummary/>
+              <WorkoutSummary/>
+              <WorkoutSummary/>
+              <WorkoutSummary/>
+              <WorkoutSummary/>
+              <WorkoutSummary/>
+            </div>
+            <div className="flex justify-center items-center w-full h-24 bg-stone-100 rounded-xl font-semibold">Keep Going!</div>
+            </div>
         </div>
     </div>
   )
